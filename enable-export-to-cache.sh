b9da604fee47b79b94de8b8c3e2df44bdb65b5a6
notgne2 <gen2@gen2.space>
2023-03-31 14:40:19 -0700

    inline enable-export-to-cache.sh

--- enable-export-to-cache.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if [ -n "${{ inputs.cache-key }}" ]; then
-    echo "post-build-hook = $GITHUB_ACTION_PATH/export-to-cache.sh" | sudo tee /etc/nix/nix.conf > /dev/null
-fi
