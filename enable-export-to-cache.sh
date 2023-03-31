3ed269bdcb06046932e77c88f02959ac08fd7118
notgne2 <gen2@gen2.space>
2023-03-31 14:34:27 -0700

    Move GitHub Action substitution out of scripts

--- enable-export-to-cache.sh
+++ enable-export-to-cache.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 if [ -n "${{ inputs.cache-key }}" ]; then
-    echo "post-build-hook = ${{ github.action_path }}/export-to-cache.sh" | sudo tee /etc/nix/nix.conf > /dev/null
+    echo "post-build-hook = $GITHUB_ACTION_PATH/export-to-cache.sh" | sudo tee /etc/nix/nix.conf > /dev/null
 fi
--- fix-nix-credentials.sh
+++ fix-nix-credentials.sh
@@ -7,23 +7,23 @@ echo "::group::Fixing Nix credential issues"
 # Allows `builtins.fetchGit' and related to work.
 mkdir -p "$HOME/.ssh"
 ssh-keyscan github.com >> "$HOME/.ssh/known_hosts"
-if [ -z "${{ inputs.ssh-key }}" ]; then
-  ssh-keygen -q -N '' -t '${{ inputs.ssh_key_format }}' -f "$HOME/.ssh/id_${{ inputs.ssh_key_format }}"
+if [ -z "$INPUTS_SSH_KEY" ]; then
+  ssh-keygen -q -N '' -t "$INPUTS_SSH_KEY_FORMAT" -f "$HOME/.ssh/id_$INPUTS_SSH_KEY_FORMAT"
 else
-  echo "${{ inputs.ssh-key }}" > "$HOME/.ssh/id_${{ inputs.ssh_key_format }}"
-  ssh-keygen -f "$HOME/.ssh/id_${{ inputs.ssh_key_format }}" -y > "$HOME/.ssh/id_${{ inputs.ssh_key_format }}.pub"
+  echo "$INPUTS_SSH_KEY" > "$HOME/.ssh/id_$INPUTS_SSH_KEY_FORMAT"
+  ssh-keygen -f "$HOME/.ssh/id_$INPUTS_SSH_KEY_FORMAT" -y > "$HOME/.ssh/id_$INPUTS_SSH_KEY_FORMAT.pub"
 fi
-chmod 600 "$HOME/.ssh/id_${{ inputs.ssh_key_format }}"
+chmod 600 "$HOME/.ssh/id_$INPUTS_SSH_KEY_FORMAT"
 ssh-agent -a "$SSH_AUTH_SOCK" > /dev/null
-ssh-add "$HOME/.ssh/id_${{ inputs.ssh_key_format }}"
+ssh-add "$HOME/.ssh/id_$INPUTS_SSH_KEY_FORMAT"
 git config --global user.email "floxuser@example.invalid"
 git config --global user.name "flox User"
 
 # Allows `builtins.fetch{url,Tarball}' and related to work:
 mkdir -p "$HOME/.config/nix";
-echo "machine api.github.com password ${{ inputs.github-access-token }}" > "$HOME/.config/nix/netrc"
-echo "machine pkgs.github.com password ${{ inputs.github-access-token }}" >> "$HOME/.config/nix/netrc"
-echo "machine github.com password ${{ inputs.github-access-token }}" >> "$HOME/.config/nix/netrc"
+echo "machine api.github.com password $INPUTS_GITHUB_ACCESS_TOKEN" > "$HOME/.config/nix/netrc"
+echo "machine pkgs.github.com password $INPUTS_GITHUB_ACCESS_TOKEN" >> "$HOME/.config/nix/netrc"
+echo "machine github.com password $INPUTS_GITHUB_ACCESS_TOKEN" >> "$HOME/.config/nix/netrc"
 
 # Close the log message group which was opened above
 echo "::endgroup::"
