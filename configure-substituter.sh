0677b9a9b45551a0e54c0eb72380cb26c0c7ebc7
Bryan Honof <bryan@floxdev.com>
2023-06-09 18:27:10 +0200

    fix: use nix instead of flox nix to avoid flox not being found

--- configure-substituter.sh
+++ configure-substituter.sh
@@ -13,18 +13,18 @@ echo "${INPUT_SUBSTITUTER_KEY}" >/tmp/secret-key
 
 # Configure Nix to use the substituter.
 {
-	EXTRA_TRUSTED_PUBLIC_KEY=$(echo "${INPUT_SUBSTITUTER_KEY}" | flox nix key convert-secret-to-public)
+	EXTRA_TRUSTED_PUBLIC_KEY=$(echo "${INPUT_SUBSTITUTER_KEY}" | nix key convert-secret-to-public)
 	echo "extra-trusted-public-keys = ${EXTRA_TRUSTED_PUBLIC_KEY}"
 	echo "extra-substituters = ${INPUT_SUBSTITUTER}"
-} >>/etc/nix/nix.conf
+} | sudo tee -a /etc/nix/nix.conf >/dev/null
 
-mkdir -p /etc/systemd/system/nix-daemon.service.d
+sudo mkdir -p /etc/systemd/system/nix-daemon.service.d
 
 printf "%s\n" \
 	'[Service]' \
 	"Environment=AWS_ACCESS_KEY_ID=${INPUT_AWS_ACCESS_KEY_ID}" \
-	"Environment=AWS_SECRET_ACCESS_KEY=${INPUT_AWS_SECRET_ACCESS_KEY}" \
-	>>/etc/systemd/system/nix-daemon.service.d/aws-credentials.conf
+	"Environment=AWS_SECRET_ACCESS_KEY=${INPUT_AWS_SECRET_ACCESS_KEY}" |
+	sudo tee -a /etc/systemd/system/nix-daemon.service.d/aws-credentials.conf >/dev/null
 
-systemctl daemon-reload
-systemctl restart nix-daemon.service
+sudo systemctl daemon-reload
+sudo systemctl restart nix-daemon.service
