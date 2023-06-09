fa2e3ff689a8c9557c152f03c2187e32aa22f195
Bryan Honof <bryan@floxdev.com>
2023-06-09 19:51:08 +0200

    refactor: print what the config substituter script is doing

--- configure-substituter.sh
+++ configure-substituter.sh
@@ -6,27 +6,30 @@ echo "::group::Setting up substituter ${INPUT_SUBSTITUTER}"
 
 echo "${INPUT_SUBSTITUTER_KEY}" >/tmp/secret-key
 
-# Populate GitHub's environment with the substituter's URL and options, and AWS's credentials.
+echo "Populating the environment with the substituter's URL and options, and AWS's credentials"
 {
 	echo "FLOX_SUBSTITUTER=${INPUT_SUBSTITUTER}${INPUT_SUBSTITUTER_OPTIONS}"
 	echo "${INPUT_AWS_ACCESS_KEY_ID}"
 	echo "${INPUT_AWS_SECRET_ACCESS_KEY}"
 } >>"${GITHUB_ENV}"
 
-# Configure Nix to use the substituter.
+echo "Making the Nix daemon aware of the substituter"
 {
 	EXTRA_TRUSTED_PUBLIC_KEY=$(echo "${INPUT_SUBSTITUTER_KEY}" | nix key convert-secret-to-public)
 	echo "extra-trusted-public-keys = ${EXTRA_TRUSTED_PUBLIC_KEY}"
 	echo "extra-substituters = ${INPUT_SUBSTITUTER}"
 } | sudo tee -a /etc/nix/nix.conf >/dev/null
 
-sudo mkdir -p /etc/systemd/system/nix-daemon.service.d
+echo "Making the Nix daemon aware of the AWS credentials"
 
+sudo mkdir -p /etc/systemd/system/nix-daemon.service.d
 printf "%s\n" \
 	'[Service]' \
 	"Environment=AWS_ACCESS_KEY_ID=${INPUT_AWS_ACCESS_KEY_ID}" \
 	"Environment=AWS_SECRET_ACCESS_KEY=${INPUT_AWS_SECRET_ACCESS_KEY}" |
 	sudo tee -a /etc/systemd/system/nix-daemon.service.d/aws-credentials.conf >/dev/null
+
+echo "Restarting the Nix daemon"
 
 sudo systemctl daemon-reload
 sudo systemctl restart nix-daemon.service
