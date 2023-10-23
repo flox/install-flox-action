843e6fd7cab45e033e8eebdcdb61cca24593a50a
Rok Garbas <rok@floxdev.com>
2023-10-23 18:13:39 +0200

    some refactoring

--- dist/index.js
+++ dist/index.js
@@ -82187,76 +82187,58 @@ async function run() {
   core.startGroup('Download & Install flox')
   const downloadUrl = await utils.getDownloadUrl(process.platform, process.arch)
   utils.exportVariableFromInput('download-url', downloadUrl)
-  await exec.exec('bash', ['-c', utils.SCRIPTS.installFlox], { detached: true })
+  await exec.exec('bash', ['-c', utils.SCRIPTS.installFlox])
   core.endGroup()
 
   core.startGroup('Configure Git')
   utils.exportVariableFromInput('git-user')
   utils.exportVariableFromInput('git-email')
-  await exec.exec('bash', ['-c', utils.SCRIPTS.configureGit], {
-    detached: true
-  })
+  await exec.exec('bash', ['-c', utils.SCRIPTS.configureGit])
   core.endGroup()
 
-  core.startGroup('Configure Github')
   const githubAccessToken = utils.exportVariableFromInput('github-access-token')
   if (githubAccessToken) {
-    await exec.exec('bash', ['-c', utils.SCRIPTS.configureGithub], {
-      detached: true
-    })
-  } else {
-    core.info(`Skip running '${utils.SCRIPTS.configureGithub}' script`)
+    core.startGroup('Configure Github')
+    await exec.exec('bash', ['-c', utils.SCRIPTS.configureGithub])
+    core.endGroup()
   }
-  core.endGroup()
 
-  core.startGroup('Configure Ssh')
   const sshKeyFormat = utils.exportVariableFromInput('ssh-key-format')
   utils.exportVariableFromInput('ssh-key')
   utils.exportVariableFromInput('ssh-auth-sock')
   if (sshKeyFormat) {
+    core.startGroup('Configure SSH')
     await exec.exec('bash', ['-c', utils.SCRIPTS.configureSsh], {
       detached: true
     })
-  } else {
-    core.info(`Skip running '${utils.SCRIPTS.configureSsh}' script`)
+    core.endGroup()
   }
-  core.endGroup()
 
-  core.startGroup('Configure Substituter')
   const substituter = utils.exportVariableFromInput('substituter')
   const substituterKey = utils.exportVariableFromInput('substituter-key')
   utils.exportVariableFromInput('substituter-options')
   if (substituter && substituterKey) {
-    await exec.exec('bash', ['-c', utils.SCRIPTS.configureSubstituter], {
-      detached: true
-    })
-  } else {
-    core.info(`Skip running '${utils.SCRIPTS.configureSubstituter}' script`)
+    core.startGroup('Configure Substituter')
+    await exec.exec('bash', ['-c', utils.SCRIPTS.configureSubstituter])
+    core.endGroup()
   }
-  core.endGroup()
 
-  core.startGroup('Configure AWS')
   const awsAccessKeyId = utils.exportVariableFromInput('aws-access-key-id')
   const awsSecretAccessKey = utils.exportVariableFromInput(
     'aws-secret-access-key'
   )
   if (awsAccessKeyId && awsSecretAccessKey) {
-    await exec.exec('bash', ['-c', utils.SCRIPTS.configureAWS], {
-      detached: true
-    })
-  } else {
-    core.info(`Skip running '${utils.SCRIPTS.configureAWS}' script`)
+    core.startGroup('Configure AWS')
+    await exec.exec('bash', ['-c', utils.SCRIPTS.configureAWS])
+    core.endGroup()
   }
-  core.endGroup()
 
-  core.startGroup('Checking flox version')
+  core.startGroup('Checking Flox Version')
   await exec.exec('flox', ['--version'])
   core.endGroup()
 
-  core.startGroup('Record Nix store paths')
-  await exec.exec('bash', ['-c', utils.SCRIPTS.recordNixStorePaths], {
-    detached: true
-  })
+  core.startGroup('Record Nix Store Paths')
+  await exec.exec('bash', ['-c', utils.SCRIPTS.recordNixStorePaths])
   core.endGroup()
 
   core.startGroup('Restore Nix Cache')
@@ -82285,15 +82267,13 @@ const exec = __nccwpck_require__(1514)
 const utils = __nccwpck_require__(1608)
 
 async function run() {
-  core.startGroup('Push Nix store paths')
   const storePathsFile = process.env['STORE_PATHS_FILE']
   const floxSubstituter = process.env['FLOX_SUBSTITUTER']
   if (storePathsFile && floxSubstituter) {
+    core.startGroup('Push Nix Store Paths')
     await exec.exec('bash', ['-c', utils.SCRIPTS.pushNewNixStorePaths])
-  } else {
-    core.debug(`Skip running '${utils.SCRIPTS.pushNewNixStorePaths}' script`)
+    core.endGroup()
   }
-  core.endGroup()
 
   core.startGroup('Save Nix Cache')
   await cache.saveCache(utils.GH_CACHE_PATHS, utils.GH_CACHE_KEY)
--- src/post.js
+++ src/post.js
@@ -4,15 +4,13 @@ const exec = require('@actions/exec')
 const utils = require('./utils')
 
 export async function run() {
-  core.startGroup('Push Nix store paths')
   const storePathsFile = process.env['STORE_PATHS_FILE']
   const floxSubstituter = process.env['FLOX_SUBSTITUTER']
   if (storePathsFile && floxSubstituter) {
+    core.startGroup('Push Nix Store Paths')
     await exec.exec('bash', ['-c', utils.SCRIPTS.pushNewNixStorePaths])
-  } else {
-    core.debug(`Skip running '${utils.SCRIPTS.pushNewNixStorePaths}' script`)
+    core.endGroup()
   }
-  core.endGroup()
 
   core.startGroup('Save Nix Cache')
   await cache.saveCache(utils.GH_CACHE_PATHS, utils.GH_CACHE_KEY)
