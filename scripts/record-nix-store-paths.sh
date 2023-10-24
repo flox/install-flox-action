eb5fcc2a5e9911d9e60b208f3c5fcdf0b3094f50
Rok Garbas <rok@floxdev.com>
2023-10-24 15:36:35 +0200

    fix count - hopefully

--- scripts/record-nix-store-paths.sh
+++ scripts/record-nix-store-paths.sh
@@ -12,5 +12,10 @@ echo "STORE_PATHS_FILE=$STORE_PATHS_FILE" >> "$GITHUB_ENV";
 find "$NIX_STORE_DIR" -maxdepth 1 -mindepth 1 -type d -o -type l  \
   |sort > "$STORE_PATHS_FILE";
 
-STORE_PATHS_FILE_COUNT="$(wc -l "$STORE_PATHS_FILE"|cut -w -f2)";
+if [[ "$RUNNER_OS" == "Linux" ]]; then
+  STORE_PATHS_FILE_COUNT="$(wc -l "$STORE_PATHS_FILE"|cut -d' ' -f1)";
+elif [[ "$RUNNER_OS" == "macOS" ]]; then
+  STORE_PATHS_FILE_COUNT="$(wc -l "$STORE_PATHS_FILE"|cut -w -f2)";
+fi
+
 echo "Recorded $STORE_PATHS_FILE_COUNT paths";
