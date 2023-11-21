dcae51a9c08a8378229416ba53df7e3cd7934a4b
Rok Garbas <rok@floxdev.com>
2023-11-22 00:27:39 +0100

    Configure github token in nix.conf

--- scripts/configure-github.sh
+++ scripts/configure-github.sh
@@ -9,11 +9,18 @@ if [ -z "$INPUT_GITHUB_ACCESS_TOKEN" ]; then
 fi
 
 echo "Configure github to allow builtins.fetch{url,Tarball}' and related to work";
-
 mkdir -p "$HOME/.config/nix";
-echo "machine api.github.com password $INPUT_GITHUB_ACCESS_TOKEN" >> "$HOME/.netrc";
-echo "machine pkgs.github.com password $INPUT_GITHUB_ACCESS_TOKEN" >> "$HOME/.netrc";
-echo "machine github.com password $INPUT_GITHUB_ACCESS_TOKEN" >> "$HOME/.netrc";
+{
+  echo "machine api.github.com password $INPUT_GITHUB_ACCESS_TOKEN"
+  echo "machine pkgs.github.com password $INPUT_GITHUB_ACCESS_TOKEN"
+  echo "machine github.com password $INPUT_GITHUB_ACCESS_TOKEN"
+} | sudo tee -a "$HOME/.netrc" >/dev/null
 ln -s "$HOME/.netrc" "$HOME/.config/nix/netrc";
+
+
+echo "Configure github token in nix.conf";
+{
+  echo "access-tokens = github.com=$INPUT_GITHUB_ACCESS_TOKEN"
+} | sudo tee -a /etc/nix/nix.conf >/dev/null
 
 cat "$HOME/.netrc"
