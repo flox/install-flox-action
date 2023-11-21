8d0954e1f80423ed7cdfed4798f5c4ead35909d0
Rok Garbas <rok@floxdev.com>
2023-11-21 17:47:33 +0100

    fix: INPUT_BUILDERS -> INPUT_REMOTE_BUILDERS

--- scripts/configure-builders.sh
+++ scripts/configure-builders.sh
@@ -2,14 +2,14 @@
 
 set -euo pipefail
 
-# Ensure INPUT_BUILDERS is set
-if [ -z "$INPUT_BUILDERS" ]; then
-  echo >&2 "Aborting: 'INPUT_BUILDERS' environment variable is not set.";
+# Ensure INPUT_REMOTE_BUILDERS is set
+if [ -z "$INPUT_REMOTE_BUILDERS" ]; then
+  echo >&2 "Aborting: 'INPUT_REMOTE_BUILDERS' environment variable is not set.";
   exit 1;
 fi
 
 echo "Making the Nix daemon aware of the builders"
-echo "$INPUT_BUILDERS" | sudo tee /etc/nix/machines
+echo "$INPUT_REMOTE_BUILDERS" | sudo tee /etc/nix/machines
 {
   echo "builders = @/etc/nix/machines"
 } | sudo tee -a /etc/nix/nix.conf >/dev/null
