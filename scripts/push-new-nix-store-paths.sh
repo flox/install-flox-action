ef02519108e02c16afd2bb18b43855c8b960d3b9
tomberek <tomberek@users.noreply.github.com>
2024-01-05 06:44:00 -0500

    Upload more to cache (#55)
    Uses the approach from
    https://www.haskellforall.com/2022/10/how-to-correctly-cache-build-time.html
    
    ---------
    
    Co-authored-by: Rok Garbas <rok@floxdev.com>

--- scripts/push-new-nix-store-paths.sh
+++ scripts/push-new-nix-store-paths.sh
@@ -9,4 +9,11 @@ if [ -z "$FLOX_SUBSTITUTER" ]; then
 fi
 
 # Allow pushing to fail.
-cat /tmp/drv-paths | xargs -I{} -r nix copy --extra-experimental-features nix-command --to "$FLOX_SUBSTITUTER" {}^* -vv||:;
+
+# copy the outputs of drv-paths
+# https://www.haskellforall.com/2022/10/how-to-correctly-cache-build-time.html
+
+if [ -f /tmp/drv-paths ]; then
+	cat /tmp/drv-paths | xargs nix-store --query --requisites --include-outputs > /tmp/dependency-paths ||:;
+	cat /tmp/dependency-paths | awk '!seen[$0]++' | xargs nix copy --extra-experimental-features nix-command --to "$FLOX_SUBSTITUTER" ||:;
+fi
