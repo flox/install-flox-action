d35f3bd92129288cb62e77a2cadae254533760dc
Bryan Honof <bryan@floxdev.com>
2024-01-12 17:58:43 +0100

    Replace legacy launchctl subcommands with newer ones (#57)
    Co-authored-by: Rok Garbas <rok@floxdev.com>
    Co-authored-by: tomberek <tomberek@users.noreply.github.com>

--- scripts/restart-nix-daemon.sh
+++ scripts/restart-nix-daemon.sh
@@ -8,9 +8,9 @@ if [[ "$RUNNER_OS" == "Linux" ]]; then
   sudo systemctl daemon-reload
   sudo systemctl restart nix-daemon.service
 elif [[ "$RUNNER_OS" == "macOS" ]]; then
-  sudo launchctl unload /Library/LaunchDaemons/org.nixos.nix-daemon.plist
-  sudo launchctl load /Library/LaunchDaemons/org.nixos.nix-daemon.plist
-  # for some reason running this twice restarts the service
+  sudo launchctl bootout system /Library/LaunchDaemons/org.nixos.nix-daemon.plist
+  sudo launchctl bootstrap system /Library/LaunchDaemons/org.nixos.nix-daemon.plist
+  # running kickstart twice ensures that we wait for service to be restarted
   sudo launchctl kickstart -k -p system/org.nixos.nix-daemon
   sudo launchctl kickstart -k -p system/org.nixos.nix-daemon
 else
