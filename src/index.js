1de39341cfc265c8975c32561cab589c29bc1d43
Rok Garbas <rok@floxdev.com>
2024-06-03 14:16:44 +0200

    remove all the configure nix bits

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 const core = require('@actions/core')
 
 const main = require('./main')
-const post = require('./post')
 
-const IsPost = core.getState('isPost')
-
-console.log(IsPost)
-
-if (!IsPost) {
-  core.saveState('isPost', 'true')
-  main.run()
-} else {
-  post.run()
-}
+main.run()
